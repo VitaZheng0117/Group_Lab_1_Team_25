01becd06e89be7e3a87885c03b9f8df8ae67fec8
Melinda Walker <walker.meli@northeastern.edu>
2025-03-12 23:54:00 -0400

    eight

---
+++ Member2/src/member2/Member2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package member2;
+
+/**
+ *
+ * @author mw554
+ */
+public class Member2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello from Member2!");
+    }
+    
+}
